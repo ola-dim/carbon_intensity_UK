60768bd7f433d120baf654814eb436240db0bba6
ola-dim <56846559+ola-dim@users.noreply.github.com>
2025-03-31 23:12:20 +0100

    etl pipeline code

--- etl.py
+++ etl.py
@@ -51,3 +51,89 @@ def transform_data(data) -> list:
     return TRANSFORMED_data
     pass
 
+def connectDB() -> tuple:
+    with open("conn.yaml", 'r') as file:
+        config = yaml.safe_load(file)
+
+        host = config.get('host')
+        user = config.get('user')
+        db = config.get('database')
+        password = config.get('password')
+        port = config.get('port')
+
+        conn = psycopg2.connect(
+            dbname=db,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        cur = conn.cursor()
+        print('Connected succesfully!')
+    return conn, cur
+
+def load_data_db(data, conn, cur) -> None:
+    data_count = 0
+    for data_point in data:
+        # Extract values from the tranformed data
+        date_rec = data_point['date']
+        time_rec = data_point['from']
+        day_recorded = data_point['day_recorded']
+        month_recorded = data_point['month_recorded']
+        dnoregion = data_point['dnoregion']
+        regionid = data_point['regionid']
+        intensity_forecast = data_point['intensity_forecast']
+        intensity_index = data_point['intensity_index']
+        biomass = data_point.get('biomass')  # Use.get() to handle missing values
+        coal = data_point.get('coal')
+        imports = data_point.get('imports')
+        gas = data_point.get('gas')
+        nuclear = data_point.get('nuclear')
+        other = data_point.get('other')
+        hydro = data_point.get('hydro')
+        solar = data_point.get('solar')
+        wind = data_point.get('wind')
+
+        # SQL query to insert data
+        insert_query = """
+            INSERT INTO carbon_intensity ("date", "from", day_recorded, month_recorded, 
+            dnoregion, region_id, intensity_forecast, intensity_index, 
+            biomass, coal, imports, gas, nuclear, other, hydro, solar, wind)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        # Execute the query with the data
+        cur.execute(insert_query, (date_rec, time_rec, day_recorded, month_recorded,
+                                   dnoregion, regionid, intensity_forecast,
+                                   intensity_index, biomass, coal, imports, gas,
+                                   nuclear, other, hydro, solar, wind))
+
+        # Count the number of records
+        data_count += 1
+
+    # Commit the changes to the database
+    conn.commit()
+    print(f"{data_count} records inserted successfully!")
+
+def load_data_csv(data, fname="carbon_intensity_data") -> None:
+    df = pd.DataFrame(data)
+    re_no = df.shape[0]
+    df.to_csv(f"{fname}.csv", index=True, index_label="ID")
+    print(f"{re_no} records successfully saved to csv.")
+
+
+if __name__ == "__main__":
+    BASE_URL = "https://api.carbonintensity.org.uk/regional/intensity"
+    start = date(2024, 1, 1)
+    end = date(2024, 1, 2)
+
+    print("Commenced Data Extraction!")
+    data = extract_data(URL=BASE_URL, start_date=start, end_date=end)
+    print("Ended Data Extraction, Commencing Transformation!")
+    transformed_data = transform_data(data=data)
+    print("Connecting to DB....")
+    conn, curr = connectDB()
+    print('Loading Data to Database...')
+    load_data_db(data=transformed_data, conn=conn, cur=curr)
+    print("saving data to csv...")
+    load_data_csv(transformed_data)
